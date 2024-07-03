d28d94a4c1ea9d5555e16636367aa10f9d5a60e3
Keegan <keeganpatton@gmail.com>
2024-07-03 16:13:37 -0500

    update app.css | add assets

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Typewriters from "./components/typewriter.js"
 // Image Asset Imports
 import topCorner from "./assets/corner.svg"
 import bottomCorner from "./assets/bottom_right_corner.svg"
+import projectsCorner from "./assets/projects_border.svg"
+import familyPic from "./assets/family.jpg"
+
 
 function App() {
   return (
@@ -14,6 +17,12 @@ function App() {
 			
 			<img src={topCorner} alt="hello" className="top-left-border"/>
 			<img src={bottomCorner} alt="yes" className="bottom-right-border"/>
+			<img src={familyPic} alt="hey" className="vignette"/>
+
+			<h1 className="projects-header">Projects</h1>
+			<img src={projectsCorner} alt="hello" className="projects-border"/>
+
+	
 	</div>
   );
 }
