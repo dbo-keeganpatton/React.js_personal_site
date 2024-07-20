c867b7898f94fb9770e49bebd5a3d053ddfa58cc
Keegan <keeganpatton@gmail.com>
2024-07-20 13:08:31 -0500

    add cursor animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import AppRoutes from "./Routes"; 
+import AnimatedCursor from "react-animated-cursor";
+
 
 
 function App() {
   return (
-	<StrictMode>
+		<StrictMode>
+
+			{/*
+				Custom Cursor 
+				Documentation: https://github.com/stephenscaff/react-animated-cursor
+			*/}
+			<AnimatedCursor
+				innerSize={5}
+				outerSize={5}
+				color='255, 255, 255'
+				outerAlpha={.3}
+				innerScale={0}
+				outerScale={8}
+				trailingSpeed={8}
+				showSystemCursor={false}
+				clickables={[
+					'a',
+					'input[type="text"]',
+					'input[type="email"]',
+					'input[type="number"]',
+					'input[type="submit"]',
+					'input[type="image"]',
+					'label[for]',
+					'select',
+					'textarea',
+					'button',
+					'.link'
+				]}
+			/>
+
 
 		<AppRoutes />
 			
